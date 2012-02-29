fbdd90cce5fc0e1c0aa0be1fe794af78c8033885
Flinou <trasheaven@hotmail.fr>
2012-02-29 20:52:31 +0100

    Fin des pages statiques

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @titre="Accueil"
   end
 
   def contact
+    @titre="Accueil"
   end
+  def about
+     @titre="Accueil"
+  end
+
 end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe PagesController do
+render_views
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    it "devrait avoir le bon titre" do
+      get 'home'
+      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+      end
+    end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+    it "devrait avoir le bon titre" do
+      get 'contact'
+      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Contact")
+
+    end
+  end
+  
+  
+  describe "GET 'about'" do
+    it "devrait réussir" do
+      get 'about'
+      response.should be_success
+    end
+    it "devrait avoir le bon titre" do
+      get 'about'
+      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | About")
+    end
+end
+end
